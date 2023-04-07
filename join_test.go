347104c791d12a7a6dbdac5002e6b7a1026328e4
dvonthenen <david.vonthenen@gmail.com>
2023-04-07 09:49:41 -0700

    Change license to Apache 2.0

--- join_test.go
+++ join_test.go
@@ -1,6 +1,7 @@
-// Copyright 2019 The dvonthenen WebSocket Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
+// Copyright 2022 The dvonthenen WebSocket Authors. All Rights Reserved.
+// Use of this source code is governed by an Apache-2.0
 // license that can be found in the LICENSE file.
+// SPDX-License-Identifier: Apache-2.0
 
 package websocket
 
